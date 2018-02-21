5983fe23f636b93dced3ae3dc11187092d796421
J. Fernando Sánchez <balkian@gmail.com>
2018-02-21 13:02:28 +0100

    Dockerized build

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,29 @@
+from python:3.5
+
+WORKDIR /usr/src/app
+
+ADD requirements.txt .
+
+RUN pip install -r requirements.txt
+
+ADD create_bibliography.py ref.bib resume-nobib.json /usr/src/app/
+
+RUN python create_bibliography.py
+
+
+from node
+
+RUN npm install -g resume-cli jsonresume-theme-elegant
+
+WORKDIR /usr/src/app
+
+COPY --from=0 /usr/src/app/resume.json /usr/src/app/
+
+RUN resume export --theme elegant --format html index.html
+ 
 from nginx
+
+COPY --from=1 /usr/src/app/index.html /usr/src/app/resume.json /usr/share/nginx/html/
 
 EXPOSE 80
 ADD . /usr/share/nginx/html/
--- create_bibliography.py
+++ create_bibliography.py
@@ -1,22 +1,31 @@
 import bibtexparser
 from bibtexparser.customization import *
+from datetime import datetime
+
 import json
+
 
 with open("resume-nobib.json") as r:
     resume = json.load(r)
+    parser = bibtexparser.bparser.BibTexParser(common_strings=True,
+                                               customization=convert_to_unicode)
+
     with open('ref.bib') as bibtex_file:
-        parser = bibtexparser.bparser.BibTexParser()
-        parser.customization = convert_to_unicode
         bibtex_str = bibtex_file.read()
 
-    bib_database = bibtexparser.loads(bibtex_str)
+    bib_database = bibtexparser.loads(bibtex_str, parser=parser)
     pubs = []
     for entry in bib_database.entries:
         pub = {}
         if "booktitle" in entry:
             pub["publisher"] = journal(entry["booktitle"])
         if "year" in entry:
-            pub["releaseDate"] = entry["year"]
+            reldate = datetime.strptime('{}-{}-{}'.format(entry.get('year', 2010),
+                                                          entry.get('month', 'January'),
+                                                          entry.get('day', 1)),
+                                        '%Y-%B-%d')
+            pub["releaseDate"] = reldate.strftime('%Y-%m-%d')
+            print(pub['releaseDate'])
         if "author" in entry:
             pub["authors"] = author(entry["author"])
         if "abstract" in entry:
